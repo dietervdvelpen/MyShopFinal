d0baf63b9795e097daf4eba41f92635cde174401
dietervdvelpen <54279593+dietervdvelpen@users.noreply.github.com>
2019-08-23 11:25:30 +0200

    b

---
+++ MyShop/MyShop.Core/Contracts/IBasketService.cs
@@ -0,0 +1,20 @@
+﻿using MyShop.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyShop.Core.Contracts
+{
+    public interface IBasketService
+    {
+        public void AddToBasket(HttpContextBase httpContext, string productId);
+        public void RemoveFromBasket(HttpContext httpContext, string itemId);
+        public List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext);
+        public BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext);
+
+
+    }
+}
